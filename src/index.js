0164da09f3ff09ecc347ef987821b1d2bc0f3bf6
Rakesh Arunachalam <rakesh@thoughtbot.com>
2020-04-12 19:01:55 +0530

    Relational data for users

--- src/index.js
+++ src/index.js
@@ -50,6 +50,7 @@ const typeDefs = `
         name: String!
         email: String!
         age: Int
+        posts: [Post!]!
     }
 
     type Post {
@@ -94,6 +95,11 @@ const resolvers = {
     Post: {
         author(parent, args, ctx, info) {
             return users.find((user) => user.id === parent.author)
+        }
+    },
+    User: {
+        posts(parent, args, ctx, info) {
+            return posts.filter((post) => post.author === parent.id)
         }
     }
 }
