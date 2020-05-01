b0b18701bd83d92b4249cce2f12c551e0577b209
Rakesh Arunachalam <rakesh@thoughtbot.com>
2020-05-01 16:03:22 +0530

    updateUser Mutation

--- src/resolvers/mutation.js
+++ src/resolvers/mutation.js
@@ -32,6 +32,27 @@ const Mutation = {
         db.comments = db.comments.filter(comment => comment.author !== args.id)
         return deleteUsers[0]
     },
+    updateUser(parent, args, { db }, info) {
+        const { id, data } = args
+        const user = db.users.find(user => user.id === id)
+        if (!user) {
+            throw new Error('User not found!')
+        }
+        if (typeof data.email === 'string') {
+            const emailTaken = db.users.some((user) => user.email === data.email)
+            if (emailTaken) {
+                throw new Error('Email taken!')
+            }
+            user.email = data.email
+        }
+        if (typeof data.name === 'string') {
+            user.name = data.name
+        }
+        if (typeof data.age !== undefined) {
+            user.age = data.age
+        }
+        return user
+    },
     createPost(parent, { data: args }, { db }, info) {
         const userExists = db.users.some((user) => user.id === args.author)
         if (!userExists) {
