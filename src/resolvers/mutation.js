60a75917860fe0de55bd7d0e8cd387d0332f265e
Rakesh Arunachalam <rakesh@thoughtbot.com>
2020-05-01 16:15:01 +0530

    updatePost mutation to update posts

--- src/resolvers/mutation.js
+++ src/resolvers/mutation.js
@@ -76,6 +76,24 @@ const Mutation = {
         db.comments = db.comments.filter(comment => comment.post !== args.id)
         return deletePosts[0]
     },
+    updatePost(parent, args, { db }, info) {
+        console.log(args)
+        const { id, data } = args
+        const post = db.posts.find(post => post.id === id)
+        if (!post) {
+            throw new Error('Post not found!')
+        }
+        if (typeof data.title === 'string') {
+            post.title = data.title
+        }
+        if (typeof data.body === 'string') {
+            post.body = data.body
+        }
+        if (typeof data.published === 'boolean') {
+            post.published = data.published
+        }
+        return post
+    },
     createComment(parent, { data: args }, { db }, info) {
         const userExists = db.users.some((user) => user.id === args.author)
         if (!userExists) {
